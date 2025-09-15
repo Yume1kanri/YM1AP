ab864fe1147e5a5a1a5a0264a1e2f43d3fb325d7
melody3210 <melody321003@gmail.com>
2025-09-15 23:38:51 +0800

    控制電梯1.3

--- js/plugins/elevator.js
+++ js/plugins/elevator.js
@@ -15,11 +15,32 @@
 window.elevatormove = function(elevatorName) {
     if (elevatorName === 'A') {
         if ($gameVariables.value(21) === $gameVariables.value(26)) {
+            $gameSwitches.setValue(25,false)
+            $gameSwitches.setValue(26,false)
+            $gameSwitches.setValue(28,true)
             console.log('樓層一樣');
         } else if ($gameVariables.value(21) > $gameVariables.value(26)) {
-            console.log('下樓');
+            $gameSwitches.setValue(26,true)
+            if(!$gameSwitches.value(21)){
+                while ($gameVariables.value(21) === $gameVariables.value(26)) {
+                    $gameVariables.setValue(21,-1)
+                    console.log($gameVariables.value(21));
+                }
+            }
+            console.log('電梯會下樓');
         } else if ($gameVariables.value(21) < $gameVariables.value(26)) {
-            console.log('上樓');
+            $gameSwitches.setValue(25,true)
+            if(!$gameSwitches.value(21)){
+                while ($gameVariables.value(21) === $gameVariables.value(26)) {
+                    $gameVariables.setValue(21,1)
+                    console.log($gameVariables.value(21));
+                }
+            }
+            console.log('電梯會上樓');
         }
     }
 };
+
+/*電梯內有人，別層有人按電梯，需要開門讓其他人上來
+所以是 [只要有人按上下樓按鈕=>電梯的移動時間就要+id:043的總時間]、[電梯在無人的狀態會以一定速率移動到玩家樓層]
+*/
